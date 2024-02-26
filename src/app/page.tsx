ccc02d507ac9c2ef160061552b1448602ed464d5
Vi-Husak <husakvitalii7@gmail.com>
2024-02-26 18:44:39 +0200

    Added main page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
+import Image from "next/image";
+
+import Button from "@/components/Button/Button";
+
 export default function Home() {
-  return <>Головна</>;
+  return (
+    <section>
+      <div className="max-w-[866px] m-auto py-28 flex justify-between gap-12 items-center">
+        <div className="flex flex-col gap-7 items-start">
+          <h2 className="font-bold text-4xl text-[#1F1E25]">Конвертер валют</h2>
+          <p className="text-[#707C87] text-xl">
+            Переважна діяльність банківської групи за останні чотири звітні
+            квартали становить 50 і більше відсотків.
+          </p>
+          <Button type="link" href="/converter">
+            Конвертувати валюту
+          </Button>
+        </div>
+        <Image
+          src="/img01.jpg"
+          width={436}
+          height={314}
+          alt=""
+          className="rounded"
+        />
+      </div>
+    </section>
+  );
 }
